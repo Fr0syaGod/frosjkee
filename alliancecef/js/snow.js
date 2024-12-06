cc20c6297e3b840a876ca975945b14c33b4c6be4
Alex <46804941+Fr0syaGod@users.noreply.github.com>
2024-12-06 09:24:32 +0200

    snow update

--- alliancecef/js/snow.js
+++ alliancecef/js/snow.js
@@ -1,7 +1,7 @@
 let snowEnabled = false;
 let snowTimer;
 let snowElements = [];
-let snowImage = "img/snowflake.png"; // Укажите путь к изображению снежинки
+let snowImage = "image/snowflake.png"; // Убедитесь, что файл существует
 let snowMax = 250;
 let snowMaxSize = 30;
 let snowMinSize = 10;
@@ -17,13 +17,8 @@ function randomMaker(range) {
 }
 
 function initSnow() {
-    if (document.body.clientHeight) {
-        marginBottom = document.body.clientHeight;
-        marginRight = document.body.clientWidth;
-    } else {
-        marginBottom = window.innerHeight;
-        marginRight = window.innerWidth;
-    }
+    marginBottom = window.innerHeight;
+    marginRight = window.innerWidth;
 
     const snowSizeRange = snowMaxSize - snowMinSize;
 
@@ -33,10 +28,11 @@ function initSnow() {
         snowflake.src = snowImage;
         snowflake.style.position = "absolute";
         snowflake.style.zIndex = "1000";
+        snowflake.style.pointerEvents = "none";
         snowflake.style.width = `${randomMaker(snowSizeRange) + snowMinSize}px`;
         snowflake.sink = sinkSpeed * parseInt(snowflake.style.width) / 5;
         snowflake.posX = randomMaker(marginRight - parseInt(snowflake.style.width));
-        snowflake.posY = randomMaker(2 * marginBottom - marginBottom - 2 * parseInt(snowflake.style.width));
+        snowflake.posY = randomMaker(-marginBottom); // Начинает сверху
         snowflake.style.left = `${snowflake.posX}px`;
         snowflake.style.top = `${snowflake.posY}px`;
         document.body.appendChild(snowflake);
@@ -53,16 +49,16 @@ function initSnow() {
 function moveSnow() {
     if (!snowEnabled) return;
 
-    for (let i = 0; i < snowMax; i++) {
+    for (let i = 0; i < snowElements.length; i++) {
         coordinates[i] += xMove[i];
         snowElements[i].posY += snowElements[i].sink;
         snowElements[i].style.left = `${snowElements[i].posX + leftRight[i] * Math.sin(coordinates[i])}px`;
         snowElements[i].style.top = `${snowElements[i].posY}px`;
 
-        if (snowElements[i].posY >= marginBottom - 2 * parseInt(snowElements[i].style.width) || 
+        if (snowElements[i].posY >= marginBottom - parseInt(snowElements[i].style.width) || 
             parseInt(snowElements[i].style.left) > (marginRight - 3 * leftRight[i])) {
             snowElements[i].posX = randomMaker(marginRight - parseInt(snowElements[i].style.width));
-            snowElements[i].posY = 0;
+            snowElements[i].posY = -marginBottom; // Возвращаем наверх
         }
     }
 
@@ -70,27 +66,25 @@ function moveSnow() {
 }
 
 function stopSnow() {
-    // Убираем снежинки с экрана
     snowElements.forEach(snowflake => {
         if (snowflake.parentNode) {
             snowflake.parentNode.removeChild(snowflake);
         }
     });
 
-    // Очищаем массив
     snowElements = [];
     clearTimeout(snowTimer);
 }
 
 // Обработчики CEF
 cef.on("start-snow", () => {
-    if (snowEnabled) return; // Если снег уже включен
+    if (snowEnabled) return;
     snowEnabled = true;
     initSnow();
 });
 
 cef.on("stop-snow", () => {
-    if (!snowEnabled) return; // Если снег уже выключен
+    if (!snowEnabled) return;
     snowEnabled = false;
     stopSnow();
 });
