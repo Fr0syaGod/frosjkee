c652a9e2363cecbfe96f74ed35ff781b64fe7856
Alex <46804941+Fr0syaGod@users.noreply.github.com>
2025-03-07 05:42:23 +0200

    addddd

--- alliancecef/js/npc-dialog.js
+++ alliancecef/js/npc-dialog.js
@@ -1,42 +1,31 @@
-let npcid = 1;
-
-
-cef.on("show-npc-dialog", (npc, title, text) => {
+cef.on("show-new-npc-dialog", (npc, title, text, buttons) => {
     let test = document.querySelectorAll('.npc-dialog-item');
     test.forEach(e => e.remove()); 
 
-    npcid = npc; 
     $(".npc-dialog").css("display", "flex"); 
     $(".npc-dialog-title").text(title); 
     $(".npc-dialog-text").text(text); 
     cef.set_focus(true); 
-});
 
+    buttons.forEach((buttonText, index) => {
+        let button = document.createElement("div");
+        button.className = "npc-dialog-item";
+        button.innerHTML = buttonText;
+        button.id = index + 1;
+
+        button.onclick = function () {
+            cef.emit("npc-dialog-action", npc, index + 1); // Передаем npc и action
+        };
+
+        document.getElementsByClassName("npc-dialog-items")[0].append(button);
+    });
+});
 
 cef.on("update-npc-text", (text) => {
     $(".npc-dialog-text").text(text); 
 });
 
-
 cef.on("hide-npc-dialog", () => {
     $(".npc-dialog").css("display", "none");
     cef.set_focus(false); 
-});
-
-
-cef.on("insert-npc-button", (index, buttonText) => {
-   
-    let button = document.createElement("div");
-    button.className = "npc-dialog-item";
-    button.innerHTML = buttonText;
-    button.id = index;
-
-    
-    button.onclick = function () {
-      
-        cef.emit("npc-dialog-action", npcid, index);
-    };
-
-    
-    document.getElementsByClassName("npc-dialog-items")[0].append(button);
 });
