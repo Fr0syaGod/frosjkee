6ecc6af699a5007aa1ce834f8a83e1dad3b58160
Alex <46804941+Fr0syaGod@users.noreply.github.com>
2025-03-07 17:07:37 +0200

    ad

--- alliancecef/js/npc-dialog.js
+++ alliancecef/js/npc-dialog.js
@@ -1,26 +1,54 @@
-cef.on("show-new-npc-dialog", (npc, title, text, buttons) => {
+let npcid = 1;
+
+// Основной диалог
+cef.on("show-npc-dialog", (npc, title, text) => {
     let test = document.querySelectorAll('.npc-dialog-item');
     test.forEach(e => e.remove()); 
-
+    npcid = npc; 
     $(".npc-dialog").css("display", "flex"); 
     $(".npc-dialog-title").text(title); 
     $(".npc-dialog-text").text(text); 
     cef.set_focus(true); 
-
-    buttons.forEach((buttonText, index) => {
-        let button = document.createElement("div");
-        button.className = "npc-dialog-item";
-        button.innerHTML = buttonText;
-        button.id = index + 1;
-
-        button.onclick = function () {
-            cef.emit("npc-dialog-action", npc, index + 1); // Передаем npc и action
-        };
-
-        document.getElementsByClassName("npc-dialog-items")[0].append(button);
-    });
 });
 
+cef.on("insert-npc-button", (index, buttonText) => {
+    let button = document.createElement("div");
+    button.className = "npc-dialog-item";
+    button.innerHTML = buttonText;
+    button.id = index;
+    
+    button.onclick = function () {
+        cef.emit("npc-dialog-action", index);
+    };
+    
+    document.getElementsByClassName("npc-dialog-items")[0].append(button);
+});
+
+// Диалог семьи
+cef.on("show-family-dialog", (npc, title, text) => {
+    let test = document.querySelectorAll('.npc-dialog-item');
+    test.forEach(e => e.remove()); 
+    npcid = npc; 
+    $(".npc-dialog").css("display", "flex"); 
+    $(".npc-dialog-title").text(title); 
+    $(".npc-dialog-text").text(text); 
+    cef.set_focus(true); 
+});
+
+cef.on("insert-family-button", (index, buttonText) => {
+    let button = document.createElement("div");
+    button.className = "npc-dialog-item";
+    button.innerHTML = buttonText;
+    button.id = index;
+    
+    button.onclick = function () {
+        cef.emit("npc-family-action", index);
+    };
+    
+    document.getElementsByClassName("npc-dialog-items")[0].append(button);
+});
+
+// Общие функции для обоих диалогов
 cef.on("update-npc-text", (text) => {
     $(".npc-dialog-text").text(text); 
 });
