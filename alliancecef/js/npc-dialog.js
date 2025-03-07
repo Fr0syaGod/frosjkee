bcbfea19a5519784e3ec4ef7a524e31e7bec1adb
Alex <46804941+Fr0syaGod@users.noreply.github.com>
2025-03-07 04:15:37 +0200

    add

--- alliancecef/js/npc-dialog.js
+++ alliancecef/js/npc-dialog.js
@@ -1,27 +1,15 @@
-let npcid = null;
+let npcid = 1;
 
 
 cef.on("show-npc-dialog", (npc, title, text) => {
     let test = document.querySelectorAll('.npc-dialog-item');
     test.forEach(e => e.remove()); 
-    
-    // ВАЖНО: Создаем локальную переменную для этого конкретного диалога
-    let currentNpcId = npc; 
-    
+
+    npcid = npc; 
     $(".npc-dialog").css("display", "flex"); 
     $(".npc-dialog-title").text(title); 
     $(".npc-dialog-text").text(text); 
     cef.set_focus(true); 
-
-    // Очищаем предыдущие обработчики
-    $(".npc-dialog-items").off('click', '.npc-dialog-item');
-    
-    // Используем делегирование событий с замыканием
-    $(".npc-dialog-items").on('click', '.npc-dialog-item', function() {
-        let index = $(this).attr('id');
-        console.log("Emitting NPC ID:", currentNpcId, "Action:", index);
-        cef.emit("npc-dialog-action", currentNpcId, index);
-    });
 });
 
 
